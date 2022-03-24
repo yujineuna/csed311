5a10979c87236f0122f3bfb738d67bfcc05e106d
dmsgk724 <dmsgk724@postech.ac.kr>
2022-03-24 23:45:39 +0900

    ecall fixed

--- Lab2/Memory.v
+++ Lab2/Memory.v
@@ -23,7 +23,7 @@ module InstMemory #(parameter MEM_DEPTH = 1024) (input reset,
       for (i = 0; i < MEM_DEPTH; i = i + 1)
           mem[i] = 32'b0;
       // Provide path of the file including instructions with binary format
-      $readmemh("C:/Users/jangyj2020/Desktop/CSED311/Lab2/loop.txt", mem);
+      $readmemh("C:/Users/dmsgk/lab_2/csed311-master/Lab2/basic_mem.txt", mem);
     end
   end
 
